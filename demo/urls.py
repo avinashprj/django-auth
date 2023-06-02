4069364d0dcfdec78aa464e4ca772bf255b81f30
avinashprj <avinashprajapati914@gmail.com>
2023-06-02 15:15:26 +0530

    feat: add init auth app

---
+++ demo/urls.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+from django.urls import path
+from django.contrib.auth import views as auth_views
+
+from users import views as user_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', user_views.home, name='home'),
+    path('register/', user_views.register, name='register'),
+]
